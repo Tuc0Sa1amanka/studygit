c15c7838501a9325bdb83150e635ffcc58622ade
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-10-18 01:04:27 +0300

    MATRIXHUM

--- matrix.hpp
+++ matrix.hpp
@@ -2,7 +2,6 @@
 #define MATRIX_HPP
 #include <cstddef>
 int ** nums (size_t m, size_t n);
-void clear  (int ** t, size_t m, size_t n);
 void input (int ** t, size_t m, size_t n);
 void out (const int * const * t,size_t m, size_t n);
 void dest (int ** t, size_t m);
