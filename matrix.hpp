b88fa255168d2a7a25a1f08dfdc81e0fe7dac016
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-10-18 00:33:37 +0300

    MTRXH

---
+++ matrix.hpp
@@ -0,0 +1,9 @@
+#ifndef MATRIX_HPP
+#define MATRIX_HPP
+#include <cstddef>
+int ** nums (size_t m, size_t n);
+void clear  (int ** t, size_t m, size_t n);
+void input (int ** t, size_t m, size_t n);
+void out (const int * const * t,size_t m, size_t n);
+void dest (int ** t, size_t m);
+#endif
