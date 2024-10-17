c1609559bbeb9e34bbd0b2554bd3513cf3a5c12a
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-10-18 00:31:57 +0300

    mainM

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
+#include "matrix.hpp"
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
-  std::cout << M << ' ' << N;
+    if(!std::cin)
+{
+    std::cerr << "ERR\n";
+    return 1;
 }
+
+  int ** t = nullptr;
+   try
+  {
+    t = nums(M, N);
+  }
+  catch(const std::bad_alloc &e)
+  {
+    std::cerr << "Not enough memory\n";
+    return 2;
+  }
+  input (t, M, N);
+  out (t, M, N);
+  clear (t, M, N);
+  dest (t, M);
+  return 0;
+}
+
+
+
