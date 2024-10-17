af614a05f59348b7e4b5e422ff01a962fc1213cc
Tuc0Sa1amanka <sparowj765@gmail.com>
2024-10-18 00:32:49 +0300

    MTRXH

---
+++ matrix.cpp
@@ -0,0 +1,55 @@
+#include "matrix.hpp"
+#include <iostream>
+
+
+void input(int** t, size_t m, size_t n)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    for (size_t j = 0; j < n; ++j)
+    {
+      std::cin >> t[i][j];
+    }
+  }
+}
+
+void output(const int * const * t, size_t m, size_t n)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    std::cout << t[i][0];
+    for (size_t j = 1; j < n; ++j)
+    {
+      std::cout << " " << t[i][j];
+    }
+    std::cout << "\n";
+  }
+}
+
+int** createTable(size_t m, size_t n)
+{
+  int** t = new int*[m];
+  size_t created = 0;
+  try
+  {
+    for (; created < m; ++created)
+    {
+      t[created] = new int[n];
+    }
+  }
+  catch (const std::bad_alloc &e)
+  {
+    dest(t, created);
+    throw;
+  }
+  return t;
+}
+
+void dest(int** t, size_t m)
+{
+  for (size_t i = 0; i < m; ++i)
+  {
+    delete[]t[i];
+  }
+  delete[]t;
+}
